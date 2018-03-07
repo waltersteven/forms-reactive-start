346d59a78c0a7d93fcd1b13231c9818599a075e7
Walter Pariona Sanchez <wltrst97@gmail.com>
2018-03-07 10:52:37 -0500

    Creating a Form in code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
 
   singupForm: FormGroup;
+
+  ngOnInit(){
+    this.singupForm = new FormGroup({
+      //adding controls
+      'username': new FormControl(null), //parameters: the initial value, validators, null for empty
+      'email': new FormControl(null),
+      'gender': new FormControl('male') //with initial value
+    }); 
+  }
+
 }
