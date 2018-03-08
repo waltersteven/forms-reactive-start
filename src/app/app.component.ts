14c711d14865c258d8eee0969f73c381f0433463
Walter Pariona Sanchez <wltrst97@gmail.com>
2018-03-08 17:02:22 -0500

    Arrays of Form Controls (FormArray)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +14,22 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.signupForm = new FormGroup({
       //adding controls
-      'username': new FormControl(null, Validators.required), //parameters: the initial value, validators, null for empty. We set the validators here because it's Reactive
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('male') //with initial value
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required), //parameters: the initial value, validators, null for empty. We set the validators here because it's Reactive
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),      
+      'gender': new FormControl('male'), //with initial value
+      'hobbies': new FormArray([]) //we initialize it with []
     }); 
   }
 
   onSubmit() {
     console.log(this.signupForm); //we got access to the form because we created from here.
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required); //null it's the default value
+    (<FormArray>this.signupForm.get('hobbies')).push(control) //first we are casting with <FormArray> and then pushing the new control
   }
 
 }
